a52aca62bfcd8cd5fd65b59a26860006dd92f90f
alaJream <jeremy.gonzales@students.dominican.edu>
2021-10-20 15:21:27 -0700

    added methods

--- ba.py
+++ ba.py
@@ -3,3 +3,26 @@ class BankAccount:
         self.name = full_name
         self.account = account_number
         self.balance = balance
+
+    def deposit(self):
+        # add parameter to take amount and add to balance
+        print("Amount Deposited: $X.XX new balance: $Y.YY")
+
+    def withdraw(self):
+        # add parameter to subtract from balance
+        print("Amount withdrawn: $X.XX new balance: $Y.YY")
+        # if user withdraw account greater than balance account 
+        print("Insufficient funds.")
+
+    def get_balance(self):
+        print(f"Welcome to Gonzales Banking, here is your current balance: {self.balance}.")
+
+    def add_interest(self):
+        # adds interest to the users balance, annual interest rate is 1%, 0.083%/month
+        # interest = balance * 0.00083
+        print("Interest rate")
+
+    def print_statement(self):
+        print(self.name)
+        print(self.account)
+        print(self.balance)
